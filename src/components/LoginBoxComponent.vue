4a06cced43cd6ef9adfae5987101f05c5d2480bc
Moises <ve11621@innovaccion.mx>
2022-11-11 13:08:42 -0600

    Token is handled successfully by the backend, JWT problems are no longer. So, now I have to focus on the queue sendings, for obtaining information, to be clever I'll create my first saga trigger pattern event

--- src/components/LoginBoxComponent.vue
+++ src/components/LoginBoxComponent.vue
@@ -1,36 +1,14 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import { useNotification } from "@kyvg/vue3-notification";
-
 const { notify } = useNotification()
-
-
 let usernameBinding = ref<string>('');
 let passwordBinding = ref<string>('');
 
-async function validateDataInputs(authenticateCallback: (usernameBinding: string, passwordBinding: string) => void) {
-    console.log(usernameBinding.value, ' ', passwordBinding.value);
-    //let jsonResponseAuthenticationOauth = {};
-    usernameBinding.value = usernameBinding.value.trim();
-    passwordBinding.value = passwordBinding.value.trim();
-    if (usernameBinding.value !== '' && passwordBinding.value !== '') {
-        let jsonResponseAuthenticationOauth = await authenticateCallback(usernameBinding.value, passwordBinding.value);
-        console.log(jsonResponseAuthenticationOauth,' here s the server response');
-    } else {
-        notify({
-            title: "Empty inputs",
-            text: "You must provide your username and password in order to log in.",
-            type: 'error'
-        });
-    }
-
-
-
-}
-
 async function authenticateCredentials(usernameBinding: string, passwordBinding: string) {
-    let requestTaken = await fetch('http://localhost:9000/login', {
+    let requestTaken = await fetch('http://localhost:8092/auth/login', {
         method: 'POST',
+       // mode:'no-cors',
         headers: {
             'Content-type': 'application/json'
         },
@@ -40,6 +18,46 @@ async function authenticateCredentials(usernameBinding: string, passwordBinding:
     let responseJson = requestTaken.json();
     return responseJson;
 }
+
+/*Just for testing the endpoint, so I'm planning on
+configuring a webclient from the authorization endpoint to handle all the requests
+from the oauth microservice*/
+async function testAuthentication(){
+    let tokenString:string=localStorage.getItem('token-auth') as string;
+    let anotherRequest=await fetch('http://localhost:8092/auth/redirect/done',{
+        method:'GET',
+        headers:{
+            'Authorization':'Bearer '.concat(JSON.parse(tokenString).token),
+            'Content-type':'application/json'
+        }
+    });
+    let anotherRequestResponse=anotherRequest.json();
+    return anotherRequestResponse;
+}
+
+async function validateDataInputs(authenticateCallback: (usernameBinding: string, passwordBinding: string) => void) {
+    console.log(usernameBinding.value, ' ', passwordBinding.value);
+    //let jsonResponseAuthenticationOauth = {};
+    usernameBinding.value = usernameBinding.value.trim();
+    passwordBinding.value = passwordBinding.value.trim();
+    if (usernameBinding.value !== '' && passwordBinding.value !== '') {
+        let jsonResponseAuthenticationOauth = await authenticateCallback(usernameBinding.value, passwordBinding.value);
+        console.log(jsonResponseAuthenticationOauth,' here s the server response');
+        localStorage.setItem('token-auth', JSON.stringify(jsonResponseAuthenticationOauth));
+        //let tokenString=localStorage.getItem('token-auth') as string;
+        
+        //console.log(JSON.parse(tokenString));
+        console.log(await testAuthentication(),' here s the endpoint response unadultered');
+    } else {
+        notify({
+            title: "Empty inputs",
+            text: "You must provide your username and password in order to log in.",
+            type: 'error'
+        });
+    }
+}
+
+
 
 </script>
 <template>
